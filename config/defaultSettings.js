b2c70069691d57d5c5a0b5a726f926fdbdfdca76
liwq <liwq@zetyun.com>
2020-11-05 10:55:21 +0800

    init

---
+++ config/defaultSettings.js
@@ -0,0 +1,18 @@
+const proSettings = {
+  "navTheme": "dark",
+  "primaryColor": "#1890ff",
+  "layout": "top",
+  "contentWidth": "Fixed",
+  "fixedHeader": false,
+  "fixSiderbar": true,
+  "menu": {
+    "locale": true
+  },
+  "title": "Ant Design Pro",
+  "pwa": false,
+  "iconfontUrl": "",
+  "splitMenus": false,
+  "footerRender": false
+};
+
+export default proSettings;
